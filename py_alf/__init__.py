826ac9e264a7a034d38a23248e3fb6f0cbf3dd9f
Jonas Schwab <jonas.schwab@physik.uni-wuerzburg.de>
2022-02-05 10:26:01 +0100

    Add comments to py_alf/__init__.py

--- py_alf/__init__.py
+++ py_alf/__init__.py
@@ -1,9 +1,19 @@
+"""
+pyALF, a Python package for the Algorithms for Lattice Fermions (ALF).
+"""
+
+# Classes
 from . alf_source import ALF_source
 from . simulation import Simulation
-from . import utils
 from . lattice import Lattice
-from . import ana
 
+# High-level analysis functions
 from . check_warmup import check_warmup
 from . check_rebin import check_rebin
 from . analysis import analysis
+
+# Module containing low-level analysis functions
+from . import ana
+
+# Module containing utility function
+from . import utils
