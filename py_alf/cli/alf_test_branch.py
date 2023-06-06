9e4bb3752230ea6b6ebc353d1c07127bb4f850d7
Jonas Schwab <jonas.schwab@physik.uni-wuerzburg.de>
2023-06-06 17:42:14 +0200

    alf_test_branch.py: Cache ALF executables

--- py_alf/cli/alf_test_branch.py
+++ py_alf/cli/alf_test_branch.py
@@ -11,19 +11,26 @@ import os
 import sys
 import argparse
 import json
+import shutil
+import tempfile
 
 import numpy as np
 from py_alf import ALF_source, Simulation
 
 
-def test_branch(alf_dir, pars, branch_R, branch_T, **kwargs):
+def test_branch(alf_dir, pars, branch_R, branch_T, tmpdir, **kwargs):
     ham_name = pars[0]
     sim_dict = pars[1]
     sim_R = Simulation(
         ALF_source(alf_dir=alf_dir, branch=branch_R),
         ham_name, sim_dict, **kwargs
         )
-    sim_R.compile()
+    executable = os.path.join(tmpdir, f'ALF_{sim_R.config.replace(" ", "_")}_reference.out')
+    if os.path.isfile(executable):
+        shutil.copy(executable, f'{alf_dir}/Prog/ALF.out')
+    else:
+        sim_R.compile()
+        shutil.copy(f'{alf_dir}/Prog/ALF.out', executable)
     sim_R.run()
     sim_R.analysis()
     obs_R = sim_R.get_obs()
@@ -33,13 +40,19 @@ def test_branch(alf_dir, pars, branch_R, branch_T, **kwargs):
         ham_name, sim_dict, **kwargs
         )
     sim_T.sim_dir = sim_T.sim_dir + '_test'
-    sim_T.compile()
+    executable = os.path.join(tmpdir, f'ALF_{sim_T.config.replace(" ", "_")}_test.out')
+    if os.path.isfile(executable):
+        shutil.copy(executable, f'{alf_dir}/Prog/ALF.out')
+    else:
+        sim_R.compile()
+        shutil.copy(f'{alf_dir}/Prog/ALF.out', executable)
+
     sim_T.run()
     sim_T.analysis()
     obs_T = sim_T.get_obs()
 
     # test_all = obs_R.equals(obs_T)
-    with open('{}.txt'.format(sim_R.sim_dir), 'w') as f:
+    with open(f'{sim_R.sim_dir}.txt', 'w', encoding='UTF-8') as f:
         test_all = True
         for name in obs_R:
             test = True
@@ -49,7 +62,7 @@ def test_branch(alf_dir, pars, branch_R, branch_T, **kwargs):
                 except TypeError:
                     pass
                 test = test and test_temp
-            f.write('{}: {}\n'.format(name, test))
+            f.write('{name}: {test}\n')
             test_all = test_all and test
 
     return test_all
@@ -109,24 +122,26 @@ if __name__ == "__main__":
         os.remove("test.txt")
 
     test_all = True
+    tmpdir = tempfile.TemporaryDirectory()
+    print(f'Caching executables in {tmpdir.name}.')
     for sim_name, sim_dict in sim_pars.items():
         test = test_branch(
-            alf_dir, sim_dict, args.branch_R, args.branch_T,
+            alf_dir, sim_dict, args.branch_R, args.branch_T, tmpdir.name,
             machine=args.machine, mpi=args.mpi, n_mpi=args.n_mpi,
             mpiexec=args.mpiexec, mpiexec_args=mpiexec_args,
             devel=args.devel
             )
-        with open('test.txt', 'a') as f:
-            f.write('{}: {}\n'.format(sim_name, test))
+        with open('test.txt', 'a', encoding='UTF-8') as f:
+            f.write(f'{sim_name}: {test}\n')
         if not test:
             test_all = False
-    with open('test.txt', 'a') as f:
-        f.write('\tTotal: {}\n'.format(test_all))
+    with open('test.txt', 'a', encoding='UTF-8') as f:
+        f.write(f'\tTotal: {test_all}\n')
     if test_all:
         print("Test sucessful")
         sys.exit(0)
     else:
         print("Test failed")
-        with open('test.txt', 'r') as f:
+        with open('test.txt', 'r', encoding='UTF-8') as f:
             print(f.read())
         sys.exit(1)
