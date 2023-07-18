ec19e5083e0b684622a5e009a7ccea7d0199a78c
Jonas Schwab <jonas.schwab@physik.uni-wuerzburg.de>
2023-07-18 15:10:30 +0200

    alf_test_branch.py: Make prepare, run and analyze steps toggelable; Simulation.compile: Ensure chose branch is checked out

--- py_alf/cli/alf_test_branch.py
+++ py_alf/cli/alf_test_branch.py
@@ -18,54 +18,63 @@ import numpy as np
 from py_alf import ALF_source, Simulation
 
 
-def test_branch(alf_dir, pars, branch_R, branch_T, tmpdir, **kwargs):
+def test_branch(alf_dir, pars, branch_R, branch_T, tmpdir,
+                prepare_run=True, run=True, analyze=True, **kwargs):
     ham_name = pars[0]
     sim_dict = pars[1]
+
     sim_R = Simulation(
         ALF_source(alf_dir=alf_dir, branch=branch_R),
         ham_name, sim_dict, **kwargs
         )
-    executable = os.path.join(tmpdir, f'ALF_{sim_R.config.replace(" ", "_")}_reference.out')
-    if os.path.isfile(executable):
-        shutil.copy(executable, f'{alf_dir}/Prog/ALF.out')
-    else:
-        sim_R.compile()
-        shutil.copy(f'{alf_dir}/Prog/ALF.out', executable)
-    sim_R.run()
-    sim_R.analysis()
-    obs_R = sim_R.get_obs()
-
     sim_T = Simulation(
         ALF_source(alf_dir=alf_dir, branch=branch_T),
         ham_name, sim_dict, **kwargs
         )
     sim_T.sim_dir = sim_T.sim_dir + '_test'
-    executable = os.path.join(tmpdir, f'ALF_{sim_T.config.replace(" ", "_")}_test.out')
-    if os.path.isfile(executable):
-        shutil.copy(executable, f'{alf_dir}/Prog/ALF.out')
-    else:
-        sim_R.compile()
-        shutil.copy(f'{alf_dir}/Prog/ALF.out', executable)
 
-    sim_T.run()
-    sim_T.analysis()
-    obs_T = sim_T.get_obs()
+    if prepare_run:
+        executable_R = os.path.join(tmpdir, f'ALF_{sim_R.config.replace(" ", "_")}_reference.out')
+        if os.path.isfile(executable_R):
+            shutil.copy(executable_R, f'{alf_dir}/Prog/ALF.out')
+        else:
+            sim_R.compile()
+            shutil.copy(f'{alf_dir}/Prog/ALF.out', executable_R)
+        sim_R.run(copy_bin=True, only_prep=True)
 
-    # test_all = obs_R.equals(obs_T)
-    with open(f'{sim_R.sim_dir}.txt', 'w', encoding='UTF-8') as f:
-        test_all = True
-        for name in obs_R:
-            test = True
-            for dat_R, dat_T in zip(obs_R[name], obs_T[name]):
-                try:
-                    test_temp = np.allclose(dat_R, dat_T)
-                except TypeError:
-                    pass
-                test = test and test_temp
-            f.write('{name}: {test}\n')
-            test_all = test_all and test
+        executable_T = os.path.join(tmpdir, f'ALF_{sim_T.config.replace(" ", "_")}_test.out')
+        if os.path.isfile(executable_T):
+            shutil.copy(executable_T, f'{alf_dir}/Prog/ALF.out')
+        else:
+            sim_T.compile()
+            shutil.copy(f'{alf_dir}/Prog/ALF.out', executable_T)
+        sim_T.run(copy_bin=True, only_prep=True)
 
-    return test_all
+    if run:
+        sim_R.run(bin_in_sim_dir=True)
+        sim_T.run(bin_in_sim_dir=True)
+
+    if analyze:
+        sim_R.analysis()
+        obs_R = sim_R.get_obs()
+        sim_T.analysis()
+        obs_T = sim_T.get_obs()
+
+        # test_all = obs_R.equals(obs_T)
+        with open(f'{sim_R.sim_dir}.txt', 'w', encoding='UTF-8') as f:
+            test_all = True
+            for name in obs_R:
+                test = True
+                for dat_R, dat_T in zip(obs_R[name], obs_T[name]):
+                    try:
+                        test_temp = np.allclose(dat_R, dat_T)
+                    except TypeError:
+                        pass
+                    test = test and test_temp
+                f.write('{name}: {test}\n')
+                test_all = test_all and test
+
+        return test_all
 
 
 def _get_arg_parser():
@@ -105,6 +114,15 @@ def _get_arg_parser():
     parser.add_argument(
         '--mpiexec_args', default='',
         help='Additional arguments to MPI executable.')
+    parser.add_argument(
+        '--no_prep', action='store_true',
+        help='Do not prepare runs, i.e. Compiling and creating directories.')
+    parser.add_argument(
+        '--no_sim', action='store_true',
+        help='Do not run ALF binary.')
+    parser.add_argument(
+        '--no_analyze', action='store_true',
+        help='Do not analyze and compare results.')
     return parser
 
 
@@ -127,21 +145,25 @@ if __name__ == "__main__":
     for sim_name, sim_dict in sim_pars.items():
         test = test_branch(
             alf_dir, sim_dict, args.branch_R, args.branch_T, tmpdir.name,
+            prepare_run=(not args.no_prep), run=(not args.no_sim),
+            analyze=(not args.no_analyze),
             machine=args.machine, mpi=args.mpi, n_mpi=args.n_mpi,
             mpiexec=args.mpiexec, mpiexec_args=mpiexec_args,
             devel=args.devel
             )
+        if not args.no_analyze:
+            with open('test.txt', 'a', encoding='UTF-8') as f:
+                f.write(f'{sim_name}: {test}\n')
+            if not test:
+                test_all = False
+    if not args.no_analyze:
         with open('test.txt', 'a', encoding='UTF-8') as f:
-            f.write(f'{sim_name}: {test}\n')
-        if not test:
-            test_all = False
-    with open('test.txt', 'a', encoding='UTF-8') as f:
-        f.write(f'\tTotal: {test_all}\n')
-    if test_all:
-        print("Test sucessful")
-        sys.exit(0)
-    else:
-        print("Test failed")
-        with open('test.txt', 'r', encoding='UTF-8') as f:
-            print(f.read())
-        sys.exit(1)
+            f.write(f'\tTotal: {test_all}\n')
+        if test_all:
+            print("Test sucessful")
+            sys.exit(0)
+        else:
+            print("Test failed")
+            with open('test.txt', 'r', encoding='UTF-8') as f:
+                print(f.read())
+            sys.exit(1)
