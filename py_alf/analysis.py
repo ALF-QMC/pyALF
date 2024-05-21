5ca2d95bafeab9e967769988c18124a3d0f584c0
Jonas Schwab <jonas.schwab@uni-wuerzburg.de>
2024-05-21 15:23:28 +0200

    Write analysis results additionally to HDF5-file results.h5

--- py_alf/analysis.py
+++ py_alf/analysis.py
@@ -1,5 +1,6 @@
 """Supplies the default analysis routine."""
 # pylint: disable=invalid-name
+# pylint: disable=protected-access
 
 import os
 import pickle
@@ -8,6 +9,7 @@ import h5py
 import numpy as np
 
 from .ana import (
+    AnalysisResultsScal,
     Parameters,
     ReadObs,
     ana_eq,
@@ -19,50 +21,12 @@ from .ana import (
     write_res_eq,
     write_res_tau,
 )
-from .exceptions import TooFewBinsError
+from .exceptions import AlreadyAnalyzed, TooFewBinsError
 
 
-def analysis(directory,
-             symmetry=None, custom_obs=None, do_tau=True, always=False):
-    """Perform analysis in the given directory.
-
-    Results are written to the pickled dictionary `res.pkl` and in plain text
-    in the folder `res/`.
-
-    Parameters
-    ----------
-    directory : path-like object
-        Directory containing Monte Carlo bins.
-    symmetry : list of functions, optional
-        List of functions reppresenting symmetry operations on lattice,
-        including unity. It is used to symmetrize lattice-type
-        observables.
-    custom_obs : dict, default=None
-        Defines additional observables derived from existing observables.
-        The key of each entry is the observable name and the value is a
-        dictionary with the format::
-
-            {'needs': some_list,
-             'kwargs': some_dict,
-             'function': some_function,}
-
-        `some_list` contains observable names to be read by
-        :class:`py_alf.ana.ReadObs`. Jackknife bins and kwargs from
-        `some_dict` are handed to `some_function` with a separate call for
-        each bin.
-    do_tau : bool, default=True
-        Analyze time-displaced correlation functions. Setting this to False
-        speeds up analysis and makes result files much smaller.
-    always : bool, default=False
-        Do not skip if parameters and bins are older than results.
-
-    """
-    # pylint: disable=too-many-locals
+def _get_obs_list(directory, always=False):
     # pylint: disable=too-many-branches
     # pylint: disable=too-many-statements
-    print(f'### Analyzing {directory} ###')
-    print(os.getcwd())
-
     par = Parameters(directory)
     if 'data.h5' in os.listdir(directory):
         if not always:
@@ -73,11 +37,10 @@ def analysis(directory,
                     - os.path.getmtime(os.path.join(directory, 'res.pkl'))
                 if d1 < 0 and d2 < 0:
                     print('already analyzed')
-                    return
+                    raise AlreadyAnalyzed
             except OSError:
                 pass
 
-        # pylint: disable=no-member
         with h5py.File(os.path.join(directory, 'data.h5'), "r") as f:
             params = {}
             for name in f['parameters']:
@@ -109,7 +72,7 @@ def analysis(directory,
 
         if N_bins < par.N_min():
             print('too few bins ', N_bins)
-            return
+            raise TooFewBinsError
     else:
         params = {}  # Stays empty, parameters are only supported with HDF5
         list_obs = []
@@ -130,11 +93,63 @@ def analysis(directory,
             elif o.endswith('_tau'):
                 list_obs.append(o)
                 list_tau.append(o)
+    return params, list_obs, list_scal, list_hist, list_eq, list_tau
+
+
+
+def analysis(directory,
+             symmetry=None, custom_obs=None, do_tau=True, always=False):
+    """
+    Perform analysis in the given directory.
+
+    Results are written to the pickled dictionary `res.pkl` and in plain text
+    in the folder `res/`.
+
+    Parameters
+    ----------
+    directory : path-like object
+        Directory containing Monte Carlo bins.
+    symmetry : list of functions, optional
+        List of functions representing symmetry operations on lattice,
+        including unity. It is used to symmetrize lattice-type
+        observables.
+    custom_obs : dict, default=None
+        Defines additional observables derived from existing observables.
+        The key of each entry is the observable name and the value is a
+        dictionary with the format::
+
+            {'needs': some_list,
+             'kwargs': some_dict,
+             'function': some_function,}
+
+        `some_list` contains observable names to be read by
+        :class:`py_alf.ana.ReadObs`. Jackknife bins and kwargs from
+        `some_dict` are handed to `some_function` with a separate call for
+        each bin.
+    do_tau : bool, default=True
+        Analyze time-displaced correlation functions. Setting this to False
+        speeds up analysis and makes result files much smaller.
+    always : bool, default=False
+        Do not skip if parameters and bins are older than results.
+    """
+    # pylint: disable=too-many-locals
+    # pylint: disable=too-many-branches
+    # pylint: disable=too-many-statements
+    print(f'### Analyzing {directory} ###')
+    print(os.getcwd())
+
+    try:
+        dic, list_obs, list_scal, list_hist, list_eq, list_tau = \
+            _get_obs_list(directory, always)
+    except (AlreadyAnalyzed, TooFewBinsError):
+        return
 
     if 'res' not in os.listdir(directory):
         os.mkdir(os.path.join(directory, 'res'))
 
-    dic = params
+    results_file = os.path.join(directory, 'results.h5')
+    with h5py.File(results_file, "w") as f:
+        f.attrs.create('program_name', 'ALF')
 
     if custom_obs is not None:
         print("Custom observables:")
@@ -154,6 +169,10 @@ def analysis(directory,
                         **obs_spec['kwargs'])
 
                 dat = error(J)
+                results = AnalysisResultsScal(
+                    "obs_name", np.nan, np.nan, dat[0], dat[1]
+                    )
+                results.write_to_hdf5(results_file)
 
                 dic[obs_name] = dat[0]
                 dic[obs_name+'_err'] = dat[1]
@@ -167,75 +186,68 @@ def analysis(directory,
     for obs_name in list_scal:
         print(obs_name)
         try:
-            sign, dat = ana_scal(directory, obs_name)
+            results = ana_scal(directory, obs_name)
         except TooFewBinsError:
             print("Too few bins, skipping.")
             continue
 
-        dic[obs_name+'_sign'] = sign[0]
-        dic[obs_name+'_sign_err'] = sign[1]
-        for i in range(len(dat)):
-            dic[obs_name+str(i)] = dat[i, 0]
-            dic[obs_name+str(i)+'_err'] = dat[i, 1]
-
-        np.savetxt(
-            os.path.join(directory, 'res', obs_name),
-            dat,
-            header='Sign: {} {}'.format(*sign)
-        )
+        results.write_to_hdf5(results_file)
+        results.append_to_flat_dict(dic)
+        results.savetxt(os.path.join(directory, 'res', obs_name))
 
     print("Histogram observables:")
     for obs_name in list_hist:
         print(obs_name)
         try:
-            sign, above, below, dat, upper, lower = ana_hist(directory, obs_name)
+            results = ana_hist(directory, obs_name)
         except TooFewBinsError:
             print("Too few bins, skipping.")
             continue
-
-        hist = {}
-        hist['dat'] = dat
-        hist['sign'] = sign
-        hist['above'] = above
-        hist['below'] = below
-        hist['upper'] = upper
-        hist['lower'] = lower
-        dic[obs_name] = hist
+        dic[obs_name] = results
 
         np.savetxt(
             os.path.join(directory, 'res', obs_name),
-            dat,
+            np.column_stack([results._values['dat'].value,
+                             results._values['dat'].error]),
             header='Sign: {} {}, above {} {}, below {} {}'.format(
-                *sign, *above, *below)
+                results._values['sign'].value, results._values['sign'].error,
+                results._values['above'].value, results._values['above'].error,
+                results._values['below'].value, results._values['below'].error
+            )
         )
 
     print("Equal time observables:")
     for obs_name in list_eq:
         print(obs_name)
         try:
-            sign, m_k, e_k, m_k_sum, e_k_sum, m_r, e_r, m_r_sum, e_r_sum, latt = \
-                ana_eq(directory, obs_name, sym=symmetry)
+            results = ana_eq(directory, obs_name, sym=symmetry)
         except TooFewBinsError:
             print("Too few bins, skipping.")
             continue
+        results.write_to_hdf5(results_file)
 
-        write_res_eq(directory, obs_name,
-                     m_k, e_k, m_k_sum, e_k_sum,
-                     m_r, e_r, m_r_sum, e_r_sum, latt)
+        write_res_eq(
+            directory, obs_name,
+            results._values['k'].value, results._values['k'].error,
+            results._values['k_traced'].value, results._values['k_traced'].error,
+            results._values['r'].value, results._values['r'].error,
+            results._values['r_traced'].value, results._values['r_traced'].error,
+            results.latt
+        )
 
-        dic[obs_name+'K'] = m_k
-        dic[obs_name+'K_err'] = e_k
-        dic[obs_name+'K_sum'] = m_k_sum
-        dic[obs_name+'K_sum_err'] = e_k_sum
-        dic[obs_name+'R'] = m_r
-        dic[obs_name+'R_err'] = e_r
-        dic[obs_name+'R_sum'] = m_r_sum
-        dic[obs_name+'R_sum_err'] = e_r_sum
+        dic[obs_name+'K'] = results._values['k'].value
+        dic[obs_name+'K_err'] = results._values['k'].error
+        dic[obs_name+'K_sum'] = results._values['k_traced'].value
+        dic[obs_name+'K_sum_err'] = results._values['k_traced'].error
+        dic[obs_name+'R'] = results._values['r'].value
+        dic[obs_name+'R_err'] = results._values['r'].error
+        dic[obs_name+'R_sum'] = results._values['r_traced'].value
+        dic[obs_name+'R_sum_err'] = results._values['r_traced'].error
         dic[obs_name+'_lattice'] = {
-            'L1': latt.L1,
-            'L2': latt.L2,
-            'a1': latt.a1,
-            'a2': latt.a2
+            'L1': results.latt.L1,
+            'L2': results.latt.L2,
+            'a1': results.latt.a1,
+            'a2': results.latt.a2
         }
 
     if do_tau:
@@ -243,24 +255,29 @@ def analysis(directory,
         for obs_name in list_tau:
             print(obs_name)
             try:
-                sign, m_k, e_k, m_r, e_r, dtau, latt = \
-                    ana_tau(directory, obs_name, sym=symmetry)
+                results = ana_tau(directory, obs_name, sym=symmetry)
             except TooFewBinsError:
                 print("Too few bins, skipping.")
                 continue
+            results.write_to_hdf5(results_file)
 
-            write_res_tau(directory, obs_name,
-                          m_k, e_k, m_r, e_r, dtau, latt)
+            write_res_tau(
+                directory, obs_name,
+                results._values['k'].value, results._values['k'].error,
+                results._values['r'].value, results._values['r'].error,
+                results._values['dtau'].value,
+                results.latt,
+            )
 
-            dic[obs_name+'K'] = m_k
-            dic[obs_name+'K_err'] = e_k
-            dic[obs_name+'R'] = m_r
-            dic[obs_name+'R_err'] = e_r
+            dic[obs_name+'K'] = results._values['k'].value
+            dic[obs_name+'K_err'] = results._values['k'].error
+            dic[obs_name+'R'] = results._values['r'].value
+            dic[obs_name+'R_err'] = results._values['r'].error
             dic[obs_name+'_lattice'] = {
-                'L1': latt.L1,
-                'L2': latt.L2,
-                'a1': latt.a1,
-                'a2': latt.a2
+                'L1': results.latt.L1,
+                'L2': results.latt.L2,
+                'a1': results.latt.a1,
+                'a2': results.latt.a2
             }
 
     with open(os.path.join(directory, 'res.pkl'), 'wb') as f:
--- py_alf/exceptions.py
+++ py_alf/exceptions.py
@@ -2,3 +2,6 @@
 
 class TooFewBinsError(Exception):
     """Triggered when observable has too few bins for analysis."""
+
+class AlreadyAnalyzed(Exception):
+    """Triggered when parameters and bins are older than analysis results."""
